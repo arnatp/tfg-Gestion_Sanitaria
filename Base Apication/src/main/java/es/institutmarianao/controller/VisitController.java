206bf6d6560a330365a0dda009feb5c679a6ac5c
arnau <arnautorresp@gmail.com>
2020-05-26 17:19:28 +0200

    Fixxed errors TODo implementations from interfaces

--- Base Apication/src/main/java/es/institutmarianao/controller/VisitController.java
+++ Base Apication/src/main/java/es/institutmarianao/controller/VisitController.java
@@ -1,43 +1,44 @@
 package es.institutmarianao.controller;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-public class VisitController {
-	@RequestMapping(value = "/patient/visit", method = RequestMethod.GET)
-	public ModelAndView orders() throws ServletException, IOException {
-		ModelAndView modelview = new ModelAndView("orders");
-		return modelview;
-	}
-
-	@RequestMapping(value = "/patient/visit", method = RequestMethod.POST)
-	public ModelAndView orders() throws ServletException, IOException {
-		ModelAndView modelview = new ModelAndView("orders");
-		return modelview;
-	}
-
-	@RequestMapping(value = "/doctor/visits", method = RequestMethod.GET)
-	public ModelAndView orders() throws ServletException, IOException {
-		ModelAndView modelview = new ModelAndView("orders");
-		return modelview;
-	}
-
-	@RequestMapping(value = "/doctor/visit", method = RequestMethod.GET)
-	public ModelAndView orders() throws ServletException, IOException {
-		ModelAndView modelview = new ModelAndView("orders");
-		return modelview;
-	}
-
-	@RequestMapping(value = "/doctor/visit", method = RequestMethod.POST)
-	public ModelAndView orders() throws ServletException, IOException {
-		ModelAndView modelview = new ModelAndView("orders");
-		return modelview;
-	}
-}
+//
+//import java.io.IOException;
+//
+//import javax.servlet.ServletException;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//public class VisitController {
+//	@RequestMapping(value = "/patient/visit", method = RequestMethod.GET)
+//	public ModelAndView orders() throws ServletException, IOException {
+//		ModelAndView modelview = new ModelAndView("orders");
+//		return modelview;
+//	}
+//
+//	@RequestMapping(value = "/patient/visit", method = RequestMethod.POST)
+//	public ModelAndView orders() throws ServletException, IOException {
+//		ModelAndView modelview = new ModelAndView("orders");
+//		return modelview;
+//	}
+//
+//	@RequestMapping(value = "/doctor/visits", method = RequestMethod.GET)
+//	public ModelAndView orders() throws ServletException, IOException {
+//		ModelAndView modelview = new ModelAndView("orders");
+//		return modelview;
+//	}
+//
+//	@RequestMapping(value = "/doctor/visit", method = RequestMethod.GET)
+//	public ModelAndView orders() throws ServletException, IOException {
+//		ModelAndView modelview = new ModelAndView("orders");
+//		return modelview;
+//	}
+//
+//	@RequestMapping(value = "/doctor/visit", method = RequestMethod.POST)
+//	public ModelAndView orders() throws ServletException, IOException {
+//		ModelAndView modelview = new ModelAndView("orders");
+//		return modelview;
+//	}
+//}
